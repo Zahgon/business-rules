9cae834415c12195a97fc80cae93fe4ef2512e72
shubhampawar-ethara <shubham.pawar@ethara.ai>
2026-04-27 11:46:17 +0530

    Commit 0

--- business_rules/operators.py
+++ business_rules/operators.py
@@ -13,21 +13,16 @@ class BaseType(object):
         self.value = self._assert_valid_value_and_cast(value)
 
     def _assert_valid_value_and_cast(self, value):
-        raise NotImplemented()
+        pass
 
     @classmethod
     def get_all_operators(cls):
-        methods = inspect.getmembers(cls)
-        return [{'name': m[0],
-                 'label': m[1].label,
-                 'input_type': m[1].input_type}
-                for m in methods if getattr(m[1], 'is_operator', False)]
+        pass
 
 
 def export_type(cls):
     """ Decorator to expose the given class to business_rules.export_rule_data. """
-    cls.export_in_rule_data = True
-    return cls
+    pass
 
 
 def type_operator(input_type, label=None,
@@ -39,20 +34,11 @@ def type_operator(input_type, label=None,
       called on them to make type errors explicit.
     """
     def wrapper(func):
-        func.is_operator = True
-        func.label = label \
-            or fn_name_to_pretty_label(func.__name__)
-        func.input_type = input_type
-
         @wraps(func)
         def inner(self, *args, **kwargs):
-            if assert_type_for_arguments:
-                args = [self._assert_valid_value_and_cast(arg) for arg in args]
-                kwargs = dict((k, self._assert_valid_value_and_cast(v))
-                              for k, v in kwargs.items())
-            return func(self, *args, **kwargs)
-        return inner
-    return wrapper
+            pass
+        pass
+    pass
 
 
 @export_type
@@ -61,39 +47,35 @@ class StringType(BaseType):
     name = "string"
 
     def _assert_valid_value_and_cast(self, value):
-        value = value or ""
-        if not isinstance(value, string_types):
-            raise AssertionError("{0} is not a valid string type.".
-                                 format(value))
-        return value
+        pass
 
     @type_operator(FIELD_TEXT)
     def equal_to(self, other_string):
-        return self.value == other_string
+        pass
 
     @type_operator(FIELD_TEXT, label="Equal To (case insensitive)")
     def equal_to_case_insensitive(self, other_string):
-        return self.value.lower() == other_string.lower()
+        pass
 
     @type_operator(FIELD_TEXT)
     def starts_with(self, other_string):
-        return self.value.startswith(other_string)
+        pass
 
     @type_operator(FIELD_TEXT)
     def ends_with(self, other_string):
-        return self.value.endswith(other_string)
+        pass
 
     @type_operator(FIELD_TEXT)
     def contains(self, other_string):
-        return other_string in self.value
+        pass
 
     @type_operator(FIELD_TEXT)
     def matches_regex(self, regex):
-        return re.search(regex, self.value)
+        pass
 
     @type_operator(FIELD_NO_INPUT)
     def non_empty(self):
-        return bool(self.value)
+        pass
 
 
 @export_type
@@ -104,36 +86,27 @@ class NumericType(BaseType):
 
     @staticmethod
     def _assert_valid_value_and_cast(value):
-        if isinstance(value, float):
-            # In python 2.6, casting float to Decimal doesn't work
-            return float_to_decimal(value)
-        if isinstance(value, integer_types):
-            return Decimal(value)
-        if isinstance(value, Decimal):
-            return value
-        else:
-            raise AssertionError("{0} is not a valid numeric type.".
-                                 format(value))
+        pass
 
     @type_operator(FIELD_NUMERIC)
     def equal_to(self, other_numeric):
-        return abs(self.value - other_numeric) <= self.EPSILON
+        pass
 
     @type_operator(FIELD_NUMERIC)
     def greater_than(self, other_numeric):
-        return (self.value - other_numeric) > self.EPSILON
+        pass
 
     @type_operator(FIELD_NUMERIC)
     def greater_than_or_equal_to(self, other_numeric):
-        return self.greater_than(other_numeric) or self.equal_to(other_numeric)
+        pass
 
     @type_operator(FIELD_NUMERIC)
     def less_than(self, other_numeric):
-        return (other_numeric - self.value) > self.EPSILON
+        pass
 
     @type_operator(FIELD_NUMERIC)
     def less_than_or_equal_to(self, other_numeric):
-        return self.less_than(other_numeric) or self.equal_to(other_numeric)
+        pass
 
 
 @export_type
@@ -142,18 +115,15 @@ class BooleanType(BaseType):
     name = "boolean"
 
     def _assert_valid_value_and_cast(self, value):
-        if type(value) != bool:
-            raise AssertionError("{0} is not a valid boolean type".
-                                 format(value))
-        return value
+        pass
 
     @type_operator(FIELD_NO_INPUT)
     def is_true(self):
-        return self.value
+        pass
 
     @type_operator(FIELD_NO_INPUT)
     def is_false(self):
-        return not self.value
+        pass
 
 @export_type
 class SelectType(BaseType):
@@ -161,32 +131,19 @@ class SelectType(BaseType):
     name = "select"
 
     def _assert_valid_value_and_cast(self, value):
-        if not hasattr(value, '__iter__'):
-            raise AssertionError("{0} is not a valid select type".
-                                 format(value))
-        return value
+        pass
 
     @staticmethod
     def _case_insensitive_equal_to(value_from_list, other_value):
-        if isinstance(value_from_list, string_types) and \
-                isinstance(other_value, string_types):
-                    return value_from_list.lower() == other_value.lower()
-        else:
-            return value_from_list == other_value
+        pass
 
     @type_operator(FIELD_SELECT, assert_type_for_arguments=False)
     def contains(self, other_value):
-        for val in self.value:
-            if self._case_insensitive_equal_to(val, other_value):
-                return True
-        return False
+        pass
 
     @type_operator(FIELD_SELECT, assert_type_for_arguments=False)
     def does_not_contain(self, other_value):
-        for val in self.value:
-            if self._case_insensitive_equal_to(val, other_value):
-                return False
-        return True
+        pass
 
 
 @export_type
@@ -195,43 +152,24 @@ class SelectMultipleType(BaseType):
     name = "select_multiple"
 
     def _assert_valid_value_and_cast(self, value):
-        if not hasattr(value, '__iter__'):
-            raise AssertionError("{0} is not a valid select multiple type".
-                                 format(value))
-        return value
+        pass
 
     @type_operator(FIELD_SELECT_MULTIPLE)
     def contains_all(self, other_value):
-        select = SelectType(self.value)
-        for other_val in other_value:
-            if not select.contains(other_val):
-                return False
-        return True
+        pass
 
     @type_operator(FIELD_SELECT_MULTIPLE)
     def is_contained_by(self, other_value):
-        other_select_multiple = SelectMultipleType(other_value)
-        return other_select_multiple.contains_all(self.value)
+        pass
 
     @type_operator(FIELD_SELECT_MULTIPLE)
     def shares_at_least_one_element_with(self, other_value):
-        select = SelectType(self.value)
-        for other_val in other_value:
-            if select.contains(other_val):
-                return True
-        return False
+        pass
 
     @type_operator(FIELD_SELECT_MULTIPLE)
     def shares_exactly_one_element_with(self, other_value):
-        found_one = False
-        select = SelectType(self.value)
-        for other_val in other_value:
-            if select.contains(other_val):
-                if found_one:
-                    return False
-                found_one = True
-        return found_one
+        pass
 
     @type_operator(FIELD_SELECT_MULTIPLE)
     def shares_no_elements_with(self, other_value):
-        return not self.shares_at_least_one_element_with(other_value)
+        pass
--- business_rules/variables.py
+++ business_rules/variables.py
@@ -14,48 +14,31 @@ class BaseVariables(object):
     """
     @classmethod
     def get_all_variables(cls):
-        methods = inspect.getmembers(cls)
-        return [{'name': m[0],
-                 'label': m[1].label,
-                 'field_type': m[1].field_type.name,
-                 'options': m[1].options,
-                } for m in methods if getattr(m[1], 'is_rule_variable', False)]
+        pass
 
 
 def rule_variable(field_type, label=None, options=None):
     """ Decorator to make a function into a rule variable
     """
-    options = options or []
     def wrapper(func):
-        if not (type(field_type) == type and issubclass(field_type, BaseType)):
-            raise AssertionError("{0} is not instance of BaseType in"\
-                    " rule_variable field_type".format(field_type))
-        func.field_type = field_type
-        func.is_rule_variable = True
-        func.label = label \
-                or fn_name_to_pretty_label(func.__name__)
-        func.options = options
-        return func
-    return wrapper
+        pass
+    pass
 
 
 def _rule_variable_wrapper(field_type, label):
-    if callable(label):
-        # Decorator is being called with no args, label is actually the decorated func
-        return rule_variable(field_type)(label)
-    return rule_variable(field_type, label=label)
+    pass
 
 def numeric_rule_variable(label=None):
-    return _rule_variable_wrapper(NumericType, label)
+    pass
 
 def string_rule_variable(label=None):
-    return _rule_variable_wrapper(StringType, label)
+    pass
 
 def boolean_rule_variable(label=None):
-    return _rule_variable_wrapper(BooleanType, label)
+    pass
 
 def select_rule_variable(label=None, options=None):
-    return rule_variable(SelectType, label=label, options=options)
+    pass
 
 def select_multiple_rule_variable(label=None, options=None):
-    return rule_variable(SelectMultipleType, label=label, options=options)
+    pass
